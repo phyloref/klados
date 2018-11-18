ade23f81ec95c63437317cb38765a0e99e975af4
Gaurav Vaidya <gaurav@ggvaidya.com>
2018-11-18 18:13:31 -0500

    Improved documentation, reorganized code and made ESLint changes.

--- js/phyx2jsonld.js
+++ js/phyx2jsonld.js
@@ -5,18 +5,38 @@
  * users to convert PHYX files to JSON-LD at the command line.
  *
  *  ./phyx2jsonld.js input.json [-o output.jsonld]
+ *
+ * Where:
+ *  - input.json is the PHYX file to be converted.
+ *  - output.jsonld is the JSON-LD file to be produced. If no output filename is
+ *    given, the output will be written to STDOUT.
+ */
+
+/* Load Javascript libraries */
+// We use 'fs' for input/output.
+const fs = require('fs');
+
+// Load yargs, a command line argument
+const yargs = require('yargs');
+
+// Get stdin, which helps read input from STDIN.
+const getStdin = require('get-stdin');
+
+/*
+ * phyx.js uses some code (in particular through phylotree.js) that expects certain
+ * Javascript libraries to be loaded via the browser using <script>. To replicate
+ * this in Node, we load them and add them to the global object.
  */
 
 // Load moment as a global variable so it can be accessed by phyx.js.
 global.moment = require('moment');
 
 // Load jQuery.extend as a global variable so it can be accessed by phyx.js.
-global.jQuery = {
-  'extend': require('extend')
-};
+global.jQuery = {};
+global.jQuery.extend = require('extend');
 
 // Load d3 as a global variable so it can be accessed by both phylotree.js (which
-// needs to add additional objects to it) and phyx (which needs to call it).
+// needs to add additional objects to it) and phyx.js (which needs to call it).
 global.d3 = require('d3');
 
 // phylotree.js does not export functions itself, but adds them to global.d3.layout.
@@ -27,15 +47,11 @@ if (!Object.prototype.hasOwnProperty.call(global.d3, 'layout')) {
 }
 require('../lib/phylotree.js/phylotree.js');
 
-// Load phyx.js.
+// Load phyx.js, our PHYX library.
 const phyx = require('../js/phyx');
 
-// We use 'fs' for input/output.
-const fs = require('fs');
-
-// Configure command line arguments.
-const args = require('yargs')
-  .usage('Usage: $0 input.json [-o output.jsonld]')
+// Configure the needed command line arguments.
+const args = yargs.usage('Usage: $0 input.json [-o output.jsonld]')
   // Set up '-o'/'--output'
   .alias('o', 'output')
   .nargs('o', 1)
@@ -43,7 +59,7 @@ const args = require('yargs')
   // Retrieve arguments.
   .argv;
 
-// Convert PHYX to JSON-LD and write out.
+// Convert PHYX to JSON-LD and write it out.
 function phyxContentToJSONLD(phyxContent) {
   const json = JSON.parse(phyxContent);
   const wrappedPhyx = new phyx.PHYXWrapper(json);
@@ -51,27 +67,34 @@ function phyxContentToJSONLD(phyxContent) {
 
   // Do we have an output filename to write to?
   if ('o' in args) {
-    fs.writeFile(args.o, jsonOutput, function(err) {
-      if (err) console.log("Could not write to ", args.o, ": ", err);
+    // Write to the output filename provided, or report an error if it could not
+    // be written.
+    fs.writeFile(args.o, jsonOutput, (err) => {
+      if (err) {
+        process.stderr.write(`Could not write to ${args.o}: ${err}\n`);
+        process.exit(1);
+      }
     });
   } else {
     // Write to the console (i.e. STDOUT).
-    console.log(jsonOutput);
+    process.stdout.write(jsonOutput);
   }
 }
 
 // Load the input file or STDIN.
 const inputFilenames = args._;
-var phyxContent;
 if (inputFilenames.length > 1) {
-  console.log("phyx2jsonld.js only supports a single input PHYX file, but ", inputFilenames.length, " provided: ", inputFilenames);
-} else if (inputFilenames.length == 0) {
-  const getStdin = require('get-stdin');
+  process.stderr.write(`phyx2jsonld.js only supports a single input PHYX file, but ${inputFilenames.length} provided: ${inputFilenames}\n`);
+  process.exit(1);
+} else if (inputFilenames.length === 0) {
   getStdin().then(str => phyxContentToJSONLD(str));
 } else {
-  fs.readFile(inputFilenames[0], 'utf8',
-      (err, data) => {
-        if (err === null) phyxContentToJSONLD(data);
-        else console.log("Error loading input file '" + inputFilenames[0] + "': " + err)
-      });
+  fs.readFile(inputFilenames[0], 'utf8', (err, data) => {
+    if (err === null) {
+      phyxContentToJSONLD(data);
+    } else {
+      process.stderr.write(`Error loading input file '${inputFilenames[0]}': ${err}`);
+      process.exit(1);
+    }
+  });
 }
