48131157710c27543ed28958bf849d9af285396d
Gaurav Vaidya <gaurav@ggvaidya.com>
2018-12-18 19:55:17 -0500

    Replaced specimen tests with expect() and given-should template.

--- test/specimens.js
+++ test/specimens.js
@@ -7,69 +7,63 @@
 const chai = require('chai');
 const phyx = require('../js/phyx');
 
-const assert = chai.assert;
+const expect = chai.expect;
 
 describe('SpecimenWrapper', function () {
   describe('#constructor', function () {
-    it('should wrap a blank object', function () {
-      const wrapper = new phyx.SpecimenWrapper({});
-      assert.exists(wrapper);
-      assert.equal(wrapper.occurrenceID, 'urn:catalog:::');
+    it('should be able to wrap an empty specimen', function () {
+      const wrapped = new phyx.SpecimenWrapper({});
+
+      expect(wrapped).to.be.an.instanceOf(phyx.SpecimenWrapper)
+      expect(wrapped.occurrenceID).to.equal('urn:catalog:::');
     });
-    it('should handle simple specimen IDs', function () {
+    it('should be able to extract an occurenceID and catalogNumber from simple specimen IDs', function () {
       const wrapper = new phyx.SpecimenWrapper({
         occurrenceID: 'Wall 2527, Fiji (uc)',
       });
-      assert.equal(wrapper.occurrenceID, 'Wall 2527, Fiji (uc)');
-      assert.equal(wrapper.catalogNumber, 'Wall 2527, Fiji (uc)');
+      expect(wrapper.occurrenceID).to.equal('Wall 2527, Fiji (uc)');
+      expect(wrapper.catalogNumber).to.equal('Wall 2527, Fiji (uc)');
     });
-    it('should handle Darwin Core doubles', function () {
+    it('should extract occurenceID, institutionCode and catalogNumber from Darwin Core doubles', function () {
       const wrapper = new phyx.SpecimenWrapper({
         occurrenceID: 'FMNH:PR 2081',
       });
-      assert.equal(wrapper.occurrenceID, 'FMNH:PR 2081');
-      assert.equal(wrapper.institutionCode, 'FMNH');
-      assert.equal(wrapper.catalogNumber, 'PR 2081');
+      expect(wrapper.occurrenceID).to.equal('FMNH:PR 2081');
+      expect(wrapper.institutionCode).to.equal('FMNH');
+      expect(wrapper.catalogNumber).to.equal('PR 2081');
     });
-    it('should handle Darwin Core triples', function () {
+    it('should extract occurenceID, institutionCode and catalogNumber from Darwin Core triples', function () {
       const wrapper = new phyx.SpecimenWrapper({
         occurrenceID: 'FMNH:PR:2081',
       });
-      assert.equal(wrapper.occurrenceID, 'FMNH:PR:2081');
-      assert.equal(wrapper.institutionCode, 'FMNH');
-      assert.equal(wrapper.collectionCode, 'PR');
-      assert.equal(wrapper.catalogNumber, '2081');
+      expect(wrapper.occurrenceID).to.equal('FMNH:PR:2081');
+      expect(wrapper.institutionCode).to.equal('FMNH');
+      expect(wrapper.collectionCode).to.equal('PR');
+      expect(wrapper.catalogNumber).to.equal('2081');
     });
-    it('should be able to split occurrenceID in different ways', function () {
-      const specimen1 = { occurrenceID: 'MVZ225749' };
-      const specimen2 = { catalogNumber: 'MVZ225749' };
-      const specimen3 = { occurrenceID: 'urn:catalog:::MVZ225749' };
-
-      const wrapper1 = new phyx.SpecimenWrapper(specimen1);
-      const wrapper2 = new phyx.SpecimenWrapper(specimen2);
-      const wrapper3 = new phyx.SpecimenWrapper(specimen3);
-
-      assert.equal(wrapper1.catalogNumber, 'MVZ225749');
-      assert.equal(wrapper2.catalogNumber, 'MVZ225749');
-      assert.equal(wrapper3.catalogNumber, 'MVZ225749');
+    it('should be able to extract the same occurrenceID from different representations', function () {
+      expect(new phyx.SpecimenWrapper({ occurrenceID: 'urn:catalog:::MVZ225749' }).occurrenceID)
+        .to.equal('urn:catalog:::MVZ225749');
+      expect(new phyx.SpecimenWrapper({ catalogNumber: 'MVZ225749' }).occurrenceID)
+        .to.equal('urn:catalog:::MVZ225749');
     });
-    it("shouldn't get confused by URNs", function () {
+    it('should not attempt to split a URN into occurenceID, institutionCode and catalogNumber', function () {
       const wrapper = new phyx.SpecimenWrapper({
         occurrenceID: 'urn:lsid:biocol.org:col:34777',
       });
-      assert.equal(wrapper.occurrenceID, 'urn:lsid:biocol.org:col:34777');
-      assert.isUndefined(wrapper.institutionCode);
-      assert.isUndefined(wrapper.collectionCode);
-      assert.isUndefined(wrapper.catalogNumber);
+      expect(wrapper.occurrenceID).to.equal('urn:lsid:biocol.org:col:34777');
+      expect(wrapper.institutionCode).to.be.undefined;
+      expect(wrapper.collectionCode).to.be.undefined;
+      expect(wrapper.catalogNumber).to.be.undefined;
     });
-    it("shouldn't get confused by URLs", function () {
+    it('should not attempt to split a URL into occurenceID, institutionCode and catalogNumber', function () {
       const wrapper = new phyx.SpecimenWrapper({
         occurrenceID: 'http://arctos.database.museum/guid/MVZ:Herp:148929?seid=886464',
       });
-      assert.equal(wrapper.occurrenceID, 'http://arctos.database.museum/guid/MVZ:Herp:148929?seid=886464');
-      assert.isUndefined(wrapper.institutionCode);
-      assert.isUndefined(wrapper.collectionCode);
-      assert.isUndefined(wrapper.catalogNumber);
+      expect(wrapper.occurrenceID).to.equal('http://arctos.database.museum/guid/MVZ:Herp:148929?seid=886464');
+      expect(wrapper.institutionCode).to.be.undefined;
+      expect(wrapper.collectionCode).to.be.undefined;
+      expect(wrapper.catalogNumber).to.be.undefined;
     });
   });
 });
