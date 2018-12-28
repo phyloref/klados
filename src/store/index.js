7a4491ed36ca43475107ca4c91f23249512573c0
Gaurav Vaidya <gaurav@ggvaidya.com>
2018-12-28 18:17:32 -0500

    Added Vuex to manage state and created a basic store.

---
+++ src/store/index.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+const debug = process.env.NODE_ENV !== 'production'
+
+export default new Vuex.Store({
+  state: {
+    CURATION_TOOL_VERSION: '0.1',
+  },
+  strict: debug,
+})
