69e3668ef5836de9680b6f828d52518dbfebfed2
Gaurav Vaidya <gaurav@ggvaidya.com>
2018-12-27 23:05:22 -0500

    Added Bootstrap 4, tried copying over top navigation bar.

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// Other libraries
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 Vue.config.productionTip = false
 
 new Vue({
