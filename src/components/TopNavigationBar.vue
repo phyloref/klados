3739544a2ccd7faad7270d8440bbf6f59c317716
Gaurav Vaidya <gaurav@ggvaidya.com>
2018-12-28 17:43:09 -0500

    Divided index.html into separate Vue components.

--- src/components/TopNavigationBar.vue
+++ src/components/TopNavigationBar.vue
@@ -1,25 +1,63 @@
 <template>
+<div>
   <!-- Navigation bar at the top of the page -->
-  <nav class="navbar navbar-default">
-    <div class="container-fluid">
-      <div class="navbar-header">
-        <a class="navbar-brand" href="index.html">Curation Tool v{{CURATION_TOOL_VERSION}}</a>
-      </div>
-      <div class="collapse navbar-collapse">
-        <ul class="nav navbar-nav navbar-right">
-          <li><a href="#" onclick="$('#about-curation-tool').modal()">About this tool</a></li>
-          <li><a href="https://www.phyloref.org/">Phyloreferencing website</a></li>
-          <li><a href="https://github.com/phyloref/curation-tool">Github repository</a></li>
-          <li><a href="https://github.com/phyloref/curation-tool/issues">Report bug</a></li>
-        </ul>
-      </div>
+  <nav class="navbar navbar-default navbar-expand-lg navbar-light">
+    <a class="navbar-brand" href="index.html">Curation Tool v{{version}}</a>
+    <div class="collapse navbar-collapse">
+      <ul class="navbar-nav">
+        <li class="nav-item"><a class="nav-link" href="#" onclick="$('#about-curation-tool').modal()">About this tool</a></li>
+        <li class="nav-item"><a class="nav-link" href="https://www.phyloref.org/">Phyloreferencing website</a></li>
+        <li class="nav-item"><a class="nav-link" href="https://github.com/phyloref/curation-tool">Github repository</a></li>
+        <li class="nav-item"><a class="nav-link" href="https://github.com/phyloref/curation-tool/issues">Report bug</a></li>
+      </ul>
     </div>
   </nav>
+
+  <!-- About-us modal dialog box: acknowledges funding and links to libraries used -->
+  <div id="about-curation-tool" class="modal fade">
+    <div class="modal-dialog modal-lg form-horizontal">
+      <div class="modal-content">
+        <!-- Header of tunit editor modal dialog box -->
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+          <h4 class="modal-title">About this tool</h4>
+        </div>
+
+        <!-- Body of the about-us modal dialog -->
+        <div class="modal-body col-md-12">
+          <p>The Phyloreferencing Curation Tool was built as part of the <a href="https://www.phyloref.org">Phyloreferencing project</a>, funded by the US National Science Foundation through collaborative grants <a href="http://www.nsf.gov/awardsearch/showAward?AWD_ID=1458484">DBI-1458484</a> and <a href="http://www.nsf.gov/awardsearch/showAward?AWD_ID=1458604">DBI-1458604</a>. See <a href="http://www.phyloref.org/about/#funding">Funding</a> for details.
+          </p>
+
+          <p>The Curation Tool uses a number of open-source libraries, including:</p>
+
+          <ul>
+            <li><a href="https://getbootstrap.com/">Bootstrap</a></li>
+            <li><a href="https://jquery.com/">jQuery</a></li>
+            <li><a href="https://jqueryui.com/">jQuery UI</a></li>
+            <li><a href="https://vuejs.org/">Vue.js</a></li>
+            <li><a href="https://github.com/veg/phylotree.js">phylotree.js</a>, published as <a href="https://doi.org/10.1186/s12859-018-2283-2">Shank <em>et al</em>, 2018</a></li>
+            <li><a href="https://d3js.org/">D3.js</a></li>
+            <li><a href="https://momentjs.com/">Moment.js</a></li>
+            <li><a href="https://github.com/eligrey/FileSaver.js">FileSaver.js</a></li>
+          </ul>
+        </div>
+
+        <!-- Footer of the about-us modal dialog -->
+        <div class="modal-footer">
+          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
 </template>
 
 <script>
 export default {
-  name: 'TopNavigationBar'
+  name: 'TopNavigationBar',
+  props: [
+    'version'
+  ]
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-// Other libraries
+// Important Bootstrap and a default theme that mimicks the Bootstrap 3 theme.
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-4-theme/dist/bootstrap-theme.min.css'
 
 Vue.config.productionTip = false
 
