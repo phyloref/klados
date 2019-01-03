a61f5250cce3652c7a74e39c25e476107be3b8d7
Gaurav Vaidya <gaurav@ggvaidya.com>
2019-01-03 17:44:47 -0500

    Incorporated non-functional sidebar.

--- src/store/index.js
+++ src/store/index.js
@@ -16,6 +16,9 @@ const debug = process.env.NODE_ENV !== 'production';
 export default new Vuex.Store({
   state: {
     CURATION_TOOL_VERSION: '0.1',
+    testcase: {
+      phylorefs: [],
+    },
   },
   modules: { phylogeny, phyloref, phyx, ui, owlterms },
   strict: debug,
