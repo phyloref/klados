b4c177efd5f5b44adbeea087254fed0e2b12c1cd
Gaurav Vaidya <gaurav@ggvaidya.com>
2019-01-04 17:57:49 -0500

    Attempt to deploy to Github Pages.

---
+++ vue.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  // Set a prefix so we can host this on Github Pages.
+  baseUrl: '/curation-tool/',
+
+  // Write out to ./docs so we can host on Github Pages without
+  // deploying separately to gh-pages.
+  outputDir: 'docs',
+};
