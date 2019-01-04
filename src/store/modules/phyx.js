b4a1ee813098e1588bbec9ce41b3bb254f016fe9
Gaurav Vaidya <gaurav@ggvaidya.com>
2019-01-04 16:06:52 -0500

    Added error checking on JSON parsing, removed Vuex namespacing.

--- src/store/modules/phyx.js
+++ src/store/modules/phyx.js
@@ -1,7 +1,6 @@
 import Vue from 'vue';
 
 export default {
-  namespaced: true,
   state: {
     currentPhyx: {
       phylorefs: [],
@@ -13,11 +12,17 @@ export default {
     },
   },
   getters: {
-    asJSON: state => JSON.stringify(state.currentPhyx, undefined, 4),
+    // Make it easier to access the key parts of the current Phyx file.
+    phylorefs: state => state.currentPhyx.phylorefs,
+    phylogenies: state => state.currentPhyx.phylogenies,
+
+    // Read as JSON.
+    getPhyxAsJSON: state => JSON.stringify(state.currentPhyx, undefined, 4),
   },
   mutations: {
-    setFromJSON(state, json) {
-      Vue.set(state, 'currentPhyx', JSON.parse(json));
+    setPhyx(state, phyx) {
+      // Replace the current PHYX file using an object.
+      Vue.set(state, 'currentPhyx', phyx);
     },
   },
 };
