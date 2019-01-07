7126a2e248c73b9e8198891a45c8597303bfecba
Gaurav Vaidya <gaurav@ggvaidya.com>
2019-01-07 18:59:41 -0500

    Renamed setPhyx methods to clearer names.

--- src/store/modules/phyx.js
+++ src/store/modules/phyx.js
@@ -6,7 +6,7 @@ export default {
       phylorefs: [],
       phylogenies: [],
     },
-    phyxAsLoaded: {
+    loadedPhyx: {
       phylorefs: [],
       phylogenies: [],
     },
@@ -16,17 +16,24 @@ export default {
     getPhyxAsJSON: state => JSON.stringify(state.currentPhyx, undefined, 4),
   },
   mutations: {
-    setPhyx(state, phyx) {
+    setCurrentPhyx(state, phyx) {
       // Replace the current Phyx file using an object. This method does NOT
       // update the loaded Phyx file, so these changes are treated as changes
       // made since the file was last loaded.
       Vue.set(state, 'currentPhyx', phyx);
     },
-    setPhyxAsLoaded(state, phyx) {
-      // Replace the current Phyx file using an object. This also updates
+    setLoadedPhyx(state, phyx) {
+      // Replace the current loaded Phyx file using an object. This also updates
       // the loaded Phyx, so we can check for changes from the loaded file.
-      Vue.set(state, 'currentPhyx', phyx);
-      Vue.set(state, 'phyxAsLoaded', JSON.parse(JSON.stringify(phyx)));
+      if (phyx === state.currentPhyx) {
+        // A common error is using the same object as the current Phyx and the
+        // loaded Phyx. In that case, we deep-copy loaded Phyx so that modifying
+        // one won't automatically modify the other.
+        Vue.set(state, 'loadedPhyx', JSON.parse(JSON.stringify(phyx)));
+      } else {
+        Vue.set(state, 'loadedPhyx', phyx);
+      }
+    },
     },
   },
 };
