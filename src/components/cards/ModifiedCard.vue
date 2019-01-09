d63e9d9756a5f534793a947f4fd4c3752d3cbfca
Gaurav Vaidya <gaurav@ggvaidya.com>
2019-01-08 19:47:17 -0500

    Added ModifiedCard and ModifiedIcon as freestanding components.

---
+++ src/components/cards/ModifiedCard.vue
@@ -0,0 +1,30 @@
+<template>
+  <div
+    v-if="dataChanged"
+    class="card mb-2 border-dark"
+  >
+    <div class="card-header bg-warning">Warning!</div>
+    <div class="card-body">
+      {{message}}
+    </div>
+  </div>
+</template>
+
+<script>
+import { _ } from 'underscore';
+
+export default {
+  name: 'ModifiedCard',
+  props: {
+    message: {
+      type: String,
+      default: "This element has changed! Please remember to save it."
+    },
+    compare: Object,
+    compareTo: Object,
+  },
+  computed: {
+    dataChanged: function () { return !_.isEqual(this.compare, this.compareTo); },
+  },
+};
+</script>
