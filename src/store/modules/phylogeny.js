3acea0b4a2e0cb59e510cb805b4019b4562cb28b
Gaurav Vaidya <gaurav@ggvaidya.com>
2019-01-11 15:44:57 -0500

    Improved method for changing phylogeny properties.

--- src/store/modules/phylogeny.js
+++ src/store/modules/phylogeny.js
@@ -1,15 +1,25 @@
+/*
+ * Store module for modifying phylogenies.
+ */
+
 import Vue from 'vue';
+import { has } from 'lodash';
 
 export default {
   mutations: {
-    setPhylogenyTitle(state, payload) {
-      Vue.set(payload.phylogeny, 'title', payload.title);
-    },
-    setPhylorefDescription(state, payload) {
-      Vue.set(payload.phylogeny, 'description', payload.description);
-    },
-    setPhylogenyNewick(state, payload) {
-      Vue.set(payload.phylogeny, 'newick', payload.newick);
+    setPhylogenyProps(state, payload) {
+      if (!has(payload, 'phylogeny')) {
+        throw new Error('setPhylogenyProps needs a phylogeny to modify using the "phylogeny" argument');
+      }
+      if (has(payload, 'title')) {
+        Vue.set(payload.phylogeny, 'title', payload.title);
+      }
+      if (has(payload, 'description')) {
+        Vue.set(payload.phylogeny, 'description', payload.description);
+      }
+      if (has(payload, 'newick')) {
+        Vue.set(payload.phylogeny, 'newick', payload.newick);
+      }
     },
   },
 };
