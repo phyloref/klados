c34f36084ba5e668852303e876c80deb3a096ae1
Gaurav Vaidya <gaurav@ggvaidya.com>
2019-01-15 14:16:34 -0500

    Documented why phylorefs are instances for now.

--- js/phyx.js
+++ js/phyx.js
@@ -1205,10 +1205,10 @@ class PhylorefWrapper {
     // Set the @id and @type.
     phylorefAsJSONLD['@id'] = phylorefURI;
 
-    // These classes are phyloreferences, and so should be classified as such.
-    // phylorefAsJSONLD['subClassOf'] = 'phyloref:Phyloreference';
-
     phylorefAsJSONLD['@type'] = [
+      // We pun this as an instance that is a Phyloreference.
+      // (We need this to ensure that the object properties that store
+      // information on specifiers will work correctly)
       'phyloref:Phyloreference',
       // Since we're writing this in RDF, just adding a '@type' of
       // phyloref:Phyloreference would imply that phylorefURI is a named
