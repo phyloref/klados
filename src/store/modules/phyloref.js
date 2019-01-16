a388eb1ddc71a95f97ddf201a99e6a377c42f68c
Gaurav Vaidya <gaurav@ggvaidya.com>
2019-01-16 18:16:59 -0500

    First attempt at modifying phyloref props.

--- src/store/modules/phyloref.js
+++ src/store/modules/phyloref.js
@@ -2,9 +2,24 @@
  * Store module for modifying phyloreferences.
  */
 
+import Vue from 'vue';
+import { has } from 'lodash';
+
 export default {
-  getters: {
-  },
   mutations: {
+    setPhylorefProps(state, payload) {
+      if (!has(payload, 'phyloref')) {
+        throw new Error('setPhylorefProps needs a phyloref to modify using the "phyloref" argument');
+      }
+      if (has(payload, 'label')) {
+        Vue.set(payload.phyloref, 'label', payload.label);
+      }
+      if (has(payload, 'cladeDefinition')) {
+        Vue.set(payload.phyloref, 'cladeDefinition', payload.cladeDefinition);
+      }
+      if (has(payload, 'curatorComments')) {
+        Vue.set(payload.phyloref, 'curatorComments', payload.curatorComments);
+      }
+    },
   },
 };
