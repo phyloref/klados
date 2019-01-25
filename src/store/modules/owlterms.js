25e28b36de7d60a81988ac42db158390361fa371
Gaurav Vaidya <gaurav@ggvaidya.com>
2019-01-25 13:22:30 -0500

    Standardized Phyx URIs to those used by phyx.js.

--- src/store/modules/owlterms.js
+++ src/store/modules/owlterms.js
@@ -1,7 +1,13 @@
 export default {
+  state: {
+    phyxBaseURI: 'http://phyloref.org/curation-tool#',
+  },
   getters: {
     getBaseURIForPhylogeny: (state, getters, rootState) => phylogeny => {
-      return `http://phyloref.org/curation-tool/#currentPhyx_phylogeny${rootState.phyx.currentPhyx.phylogenies.indexOf(phylogeny)}`;
+      return `http://phyloref.org/curation-tool#phylogeny${rootState.phyx.currentPhyx.phylogenies.indexOf(phylogeny)}`;
+    },
+    getBaseURIForPhyloref: (state, getters, rootState) => phyloref => {
+      return `http://phyloref.org/curation-tool#phyloref${rootState.phyx.currentPhyx.phylorefs.indexOf(phyloref)}`;
     },
   },
 };
