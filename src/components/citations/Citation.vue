15c69b6664180999f2f12d41362cfc59cbf161f9
Gaurav Vaidya <gaurav@ggvaidya.com>
2019-04-30 15:19:23 -0400

    Expanded citation elements, fixed formatting.

--- src/components/citations/Citation.vue
+++ src/components/citations/Citation.vue
@@ -5,18 +5,36 @@
     </label>
     <div class="col-md-10">
       <template v-if="citations.length === 0">
-        <a href="javascript:;" @click="citations.push({})">Add citation</a>
+        <a class="form-control-plaintext" href="javascript:;" @click="citations.push({})">Add citation</a>
       </template>
       <template v-for="(citation, citationIndex) of citations">
         <div class="input-group">
-          <input type="text" disabled class="form-control" placeholder="Citation" :value="wrappedCitation(citation).toString()">
+          <input @click="toggleCitationExpanded(citationIndex)" type="text" readonly class="form-control hand-cursor" placeholder="Citation" :value="wrappedCitation(citation).toString()">
           <div class="input-group-append">
+            <a v-if="wrappedCitation(citation).firstURL" class="btn btn-primary" target="_blank" :href="wrappedCitation(citation).firstURL">Open in new window</a>
             <a class="btn btn-secondary" @click="toggleCitationExpanded(citationIndex)" href="javascript:;">Expand</a>
           </div>
         </div>
         <div class="card mt-1" v-if="citationsExpanded.includes(citationIndex)">
           <div class="card-body">
             <h5 class="card-title">Citation details</h5>
+
+            <!-- Citation type -->
+            <div class="form-group row">
+              <label class="col-form-label col-md-2" for="citation-type">Citation type</label>
+              <div class="col-md-10">
+                <select id="citation-type" class="form-control"
+                  v-model="citation.type"
+                  @change="updateCitations()"
+                >
+                  <option value="journal">Journal</option>
+                  <option value="book">Book</option>
+                  <option value="book_section">Book section</option>
+                </select>
+              </div>
+            </div>
+
+            <!-- Authors (one per line) -->
             <div class="form-group row">
               <label class="col-form-label col-md-2" for="authors">Authors (one per line)</label>
               <div class="col-md-10">
@@ -27,6 +45,8 @@
                 ></textarea>
               </div>
             </div>
+
+            <!-- Title -->
             <div class="form-group row">
               <label class="col-form-label col-md-2" for="title">Title</label>
               <div class="col-md-10">
@@ -37,6 +57,89 @@
                 />
               </div>
             </div>
+
+            <!-- Only for journal entries! -->
+            <template v-if="citation.type === 'journal'">
+
+              <!-- Journal title -->
+              <div class="form-group row">
+                <label class="col-form-label col-md-2" for="journal-title">Journal title</label>
+                <div class="col-md-10">
+                  <input id="journal-title" type="text" class="form-control"
+                    placeholder="Enter journal title here"
+                    v-model="wrappedCitation(citation).journal.name"
+                    @change="updateCitations()"
+                  />
+                </div>
+              </div>
+
+              <!-- Journal volume -->
+              <div class="form-group row">
+                <label class="col-form-label col-md-2" for="journal-volume">Volume</label>
+                <div class="col-md-10">
+                  <input id="journal-volume" type="text" class="form-control"
+                    placeholder="Enter journal volume number here"
+                    v-model="wrappedCitation(citation).journal.volume"
+                    @change="updateCitations()"
+                  />
+                </div>
+              </div>
+
+              <!-- Journal pages -->
+              <div class="form-group row">
+                <label class="col-form-label col-md-2" for="journal-pages">Pages</label>
+                <div class="col-md-10">
+                  <input id="journal-pages" type="text" class="form-control"
+                    placeholder="Enter journal pages here"
+                    v-model="wrappedCitation(citation).journal.pages"
+                    @change="updateCitations()"
+                  />
+                </div>
+              </div>
+
+              <!-- ISBNs (one per line) -->
+              <div class="form-group row">
+                <label class="col-form-label col-md-2" for="isbns">ISBNs (one per line)</label>
+                <div class="col-md-10">
+                  <div class="input-group">
+                    <textarea id="isbns" rows="1" class="form-control"
+                      placeholder="Enter ISBNs here"
+                      :value="wrappedCitation(wrappedCitation(citation).journal).isbns.join('\n')"
+                      @change="wrappedCitation(wrappedCitation(citation).journal).isbns = $event.target.value.split(/\s*\n\s*/); updateCitations()"
+                    ></textarea>
+                    <div class="input-group-append">
+                      <a class="btn btn-outline-secondary align-middle" target="_blank" style="vertical-align: middle"
+                        :href="'https://www.worldcat.org/search?q=bn%3A' + wrappedCitation(wrappedCitation(citation).journal).isbns[0]"
+                        :class="{disabled: (wrappedCitation(wrappedCitation(citation).journal).isbns || []).length === 0}"
+                        >Open in new window</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <!-- ISBNs (one per line) -->
+              <div class="form-group row">
+                <label class="col-form-label col-md-2" for="issns">ISSNs (one per line)</label>
+                <div class="col-md-10">
+                  <div class="input-group">
+                    <textarea id="issns" rows="1" class="form-control"
+                      placeholder="Enter ISSNs here"
+                      :value="wrappedCitation(wrappedCitation(citation).journal).issns.join('\n')"
+                      @change="wrappedCitation(wrappedCitation(citation).journal).issns = $event.target.value.split(/\s*\n\s*/); updateCitations()"
+                    ></textarea>
+                    <div class="input-group-append">
+                      <a class="btn btn-outline-secondary align-middle" target="_blank" style="vertical-align: middle"
+                        :href="'https://www.worldcat.org/search?q=n2%3A' + wrappedCitation(wrappedCitation(citation).journal).issns[0]"
+                        :class="{disabled: (wrappedCitation(wrappedCitation(citation).journal).issns || []).length === 0}"
+                        >Open in new window</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+            </template>
+
+            <!-- DOIs (one per line) -->
             <div class="form-group row">
               <label class="col-form-label col-md-2" for="doi">DOIs (one per line)</label>
               <div class="col-md-10">
@@ -87,12 +190,24 @@ export default {
     }
   },
   data() {
+    // Set up a citations variable that has to be an array. It may actually be
+    // a single object in the JSON file; in that case, we convert it into an
+    // array.
+    let citations;
+    if (Array.isArray(this.object[this.citationKey])) {
+      citations = this.object[this.citationKey];
+    } else if(isEmpty(this.object[this.citationKey])) {
+      citations = [];
+    } else {
+      citations = [this.object[this.citationKey]];
+    }
+
     return {
       // Which citations have been expanded (by index).
       citationsExpanded: [],
 
       // Copy of the citations provided to this component.
-      citations: cloneDeep(this.object[this.citationKey] || []),
+      citations,
 
       // Items used to store new items.
       newDOI: "",
@@ -136,3 +251,9 @@ export default {
   }
 };
 </script>
+
+<style>
+.hand-cursor {
+  cursor: pointer;
+}
+</style>
