89a740cd3e2e2668f6dbb2af46b6623208c65563
Gaurav Vaidya <gaurav@ggvaidya.com>
2019-04-30 15:16:39 -0400

    Expanded Citation class: toString, ISBNs, ISSNs and firstURL.

--- src/store/modules/citations.js
+++ src/store/modules/citations.js
@@ -11,20 +11,51 @@
  * except for the Citation.JS library (https://citation.js.org/), but I haven't been
  * able to get it working on Vue JS yet. Therefore, mostly, I've implemented a
  * basic CitationWrapper here, we'll replace it with Citations.JS or another proper
- * library when we can.
+ * library when we can. We might switch over to CSL-JSON eventually (see issue
+ * https://github.com/phyloref/clade-ontology/issues/69).
  */
 
 import Vue from 'vue';
-import { has } from 'lodash';
+import { has, isEmpty } from 'lodash';
 
 class CitationWrapper {
+  // Wraps a Citation in a Phyx document. Should be moved to phyx.js.
+
   constructor(citation) {
     // Store the citation we're wrapping.
     this.citation = citation;
   }
 
   toString() {
-    return JSON.stringify(this.citation);
+    if (!this.citation) return '(undefined)';
+
+    // Returns a string representation of this citation.
+    // TODO add editors
+    let authors = this.authorsAsStrings;
+    if (authors.length > 2) authors = [`${authors[0]} et al`];
+    let authorsAndTitle = `${authors.join(' and ')} (${this.citation.year}) ${this.citation.title}`;
+    if (has(this.citation, 'section_title')) {
+      authorsAndTitle += ` (section: ${this.citation.section_title})`;
+    }
+
+    let additionalInfo = ' ';
+    if (has(this.citation, 'figure')) additionalInfo += ` fig ${this.citation.figure}`;
+
+    // Add DOIs and URLs.
+    additionalInfo += this.dois.map(doi => ` doi: ${doi}`).join('');
+    additionalInfo += this.urls.map(url => ` URL: ${url}`).join('');
+
+    additionalInfo += this.isbns.map(isbn => ` ISBN: ${isbn}`).join('');
+
+    if (has(this.citation, 'journal')) {
+      const journal = this.citation.journal;
+      const journalIssue = (has(journal, 'number')) ? `:${journal.number}` : '';
+      additionalInfo += this.issns.map(issn => `ISSN: ${issn} `).join('');
+      return `${authorsAndTitle} ${journal.name} ${journal.volume}${journalIssue} ${journal.pages}${additionalInfo}`;
+    }
+
+    // Must be a book or book section.
+    return `${authorsAndTitle} ${this.citation.publisher}, ${this.citation.city}${additionalInfo}`;
   }
 
   get authors() {
@@ -74,9 +105,13 @@ class CitationWrapper {
     return [];
   }
 
+  set identifiers(identifiers) {
+    this.citation.identifier = identifiers;
+  }
+
   get dois() {
     return this.identifiers.map((identifier) => {
-      if (has(identifier, 'type') && identifier.type === 'doi' && has(identifier, 'id')) {
+      if (has(identifier, 'type') && identifier.type === 'doi' && has(identifier, 'id') && !isEmpty(identifier.id)) {
         return [identifier.id];
       }
 
@@ -84,10 +119,73 @@ class CitationWrapper {
     }).reduce((acc, val) => acc.concat(val), []);
   }
 
+  get isbns() {
+    return this.identifiers.map((identifier) => {
+      if (has(identifier, 'type') && identifier.type === 'isbn' && has(identifier, 'id') && !isEmpty(identifier.id)) {
+        return [identifier.id];
+      }
+
+      return [];
+    }).reduce((acc, val) => acc.concat(val), []);
+  }
+
+  set isbns(isbns) {
+    this.identifiers = this.identifiers
+      .filter(identifier => has(identifier, 'type') && identifier.type !== 'isbn')
+      .concat(isbns.map(isbn => ({ type: 'isbn', id: isbn })));
+  }
+
+  get issns() {
+    return this.identifiers.map((identifier) => {
+      if (has(identifier, 'type') && identifier.type === 'issn' && has(identifier, 'id') && !isEmpty(identifier.id)) {
+        return [identifier.id];
+      }
+
+      return [];
+    }).reduce((acc, val) => acc.concat(val), []);
+  }
+
+  set issns(issns) {
+    this.identifiers = this.identifiers
+      .filter(identifier => has(identifier, 'type') && identifier.type !== 'issn')
+      .concat(issns.map(issn => ({ type: 'issn', id: issn })));
+  }
+
   get firstDOI() {
     const dois = this.dois || [];
     if (dois.length > 0) return dois[0];
     return undefined;
+  }
+
+  get firstURL() {
+    if (this.urls.length > 0) return this.urls[0];
+
+    // If we don't have a URL, look for a DOI.
+    if (this.firstDOI) return `http://doi.org/${encodeURI(this.firstDOI)}`;
+
+    // TODO: Look for ISBN.
+    // TODO: If all else fails, try https://search.crossref.org/?q={title}
+
+    return undefined;
+  }
+
+  get journal() {
+    if (has(this.citation, 'journal')) return this.citation.journal;
+    Vue.set(this.citation, 'journal', {});
+    return this.citation.journal;
+  }
+
+  get urls() {
+    if (has(this.citation, 'link')) {
+      return this.citation.link
+        .filter(link => has(link, 'url'))
+        .map(link => link.url);
+    }
+    return [];
+  }
+
+  set urls(urls) {
+    Vue.set(this.citation, 'link', urls.map(url => ({ url })));
   }
 }
 
