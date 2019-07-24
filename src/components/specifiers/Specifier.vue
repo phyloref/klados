57419e565534dadf2588beb7413960187ac0ee47
Gaurav Vaidya <gaurav@ggvaidya.com>
2019-07-24 18:44:45 -0400

    Fixed bug in retrieving nomenclatural code.

--- src/components/specifiers/Specifier.vue
+++ src/components/specifiers/Specifier.vue
@@ -384,8 +384,7 @@ export default {
   computed: {
     nomenCodes: () => TaxonNameWrapper.getNomenclaturalCodes(),
     nomenclaturalCodeObj() {
-      return TaxonNameWrapper.getNomenCodeAsObject(this.enteredNomenclaturalCode) ||
-        TaxonNameWrapper.getNomenCodeAsObject(TaxonNameWrapper.NAME_IN_UNKNOWN_CODE);
+      return TaxonNameWrapper.getNomenCodeAsObject(this.enteredNomenclaturalCode || TaxonNameWrapper.NAME_IN_UNKNOWN_CODE);
     },
     specifier() {
       // Check the specifierClass before we figure out how to construct the
