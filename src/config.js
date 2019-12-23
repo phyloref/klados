7599bd4cd16f1beb972df46f307a8fb54dc8ac07
Gaurav Vaidya <gaurav@renci.org>
2019-12-23 15:28:14 -0500

    Updated JPhyloRef submission URL to use localhost.

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   // URL for submitting jobs to JPhyloRef for processing.
-  JPHYLOREF_SUBMISSION_URL: 'https://phyloref.rc.ufl.edu/hooks/reason',
+  JPHYLOREF_SUBMISSION_URL: 'http://localhost:34214/reason',
 
   // X-Hub-Signature secret used to communicate with JPhyloRef.
   JPHYLOREF_X_HUB_SIGNATURE_SECRET: 'undefined',
