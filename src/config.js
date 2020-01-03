911eac845bd1703230ea3a87e95cd4197ff4e6ed
Gaurav Vaidya <gaurav@renci.org>
2020-01-03 00:48:00 -0500

    Updated JPhyloRef submission URL to HTTPS.

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   // URL for submitting jobs to JPhyloRef for processing.
-  JPHYLOREF_SUBMISSION_URL: 'http://reasoner.phyloref.org/reason',
+  JPHYLOREF_SUBMISSION_URL: 'https://reasoner.phyloref.org/reason',
 
   // X-Hub-Signature secret used to communicate with JPhyloRef.
   JPHYLOREF_X_HUB_SIGNATURE_SECRET: 'undefined',
