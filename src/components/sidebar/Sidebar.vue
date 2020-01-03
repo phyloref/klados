47fc2eed5a690fb3998f5170a8c434429fb1c300
Gaurav Vaidya <gaurav@renci.org>
2020-01-02 23:11:51 -0500

    Remove example file that hasn't been converted into the new format.

--- src/components/sidebar/Sidebar.vue
+++ src/components/sidebar/Sidebar.vue
@@ -248,11 +248,11 @@ export default {
         {
           url: 'examples/fisher_et_al_2007.json',
           title: 'Fisher et al, 2007',
-        },
+        },/*
         {
           url: 'examples/hillis_and_wilcox_2005.json',
           title: 'Hillis and Wilcox, 2005',
-        },
+        },*/
         {
           url: 'examples/brochu_2003.json',
           title: 'Brochu 2003',
