6b44cda725c6c7030b9f7ceca40e19654b976594
Gaurav Vaidya <gaurav@renci.org>
2020-01-03 00:26:28 -0500

    Remove "Add specifier" button entirely from the sidebar.

--- src/components/sidebar/Sidebar.vue
+++ src/components/sidebar/Sidebar.vue
@@ -154,14 +154,6 @@
             >
               &#9679; <strong>External:</strong> {{ getSpecifierLabel(specifier) }}
             </a>
-            <!--
-            <a
-              href="javascript: void(0)"
-              class="list-group-item list-group-item-action"
-              @click="$store.commit('addSpecifier', { phyloref })"
-            >
-              &#9679; <em>Add specifier</em>
-            </a> -->
           </template>
         </template>
 
