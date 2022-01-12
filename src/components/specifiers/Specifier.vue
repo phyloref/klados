9ec9086c888ef3fc09bb5a68b2bf28230aec8c5a
Gaurav Vaidya <gaurav@ggvaidya.com>
2022-01-12 01:33:45 -0500

    Fixed some phyx.js API methods that have been renamed.

--- src/components/specifiers/Specifier.vue
+++ src/components/specifiers/Specifier.vue
@@ -407,7 +407,7 @@ export default {
   computed: {
     nomenCodes: () => TaxonNameWrapper.getNomenclaturalCodes(),
     nomenclaturalCodeObj() {
-      return TaxonNameWrapper.getNomenCodeAsObject(this.enteredNomenclaturalCode);
+      return TaxonNameWrapper.getNomenCodeDetails(this.enteredNomenclaturalCode);
     },
     specifier() {
       // Check the specifierClass before we figure out how to construct the
