a5daacb919d44be87a5c88b1f4897848f785cd25
Gaurav Vaidya <gaurav@ggvaidya.com>
2022-06-14 23:09:25 -0400

    Reinstalled @vue/cli-plugin-unit-test.

--- src/__tests__/App.spec.js
+++ src/__tests__/App.spec.js
@@ -1,12 +1,11 @@
-import { mount } from '@vue/test-utils';
+import { shallowMount } from '@vue/test-utils';
 import App from '../App.vue';
 
 describe('App', () => {
     describe('Integration tests', () => {
         describe('Try demo run', () => {
-            let wrapper;
             test('is initially loaded', () => {
-                wrapper = mount(App);
+                const wrapper = shallowMount(App);
                 const navBarTitle = wrapper.findComponent("nav.navbar a");
                 expect(navBarTitle.text()).toEqual("Klados");
                 expect(navBarTitle.get("[href]").text()).toEqual("index.html");
